d53cf10330eaa13810d3f252f533cf64f8e123ac
master <josemartin.lopez.blanco@gmail.com>
2019-02-06 19:16:11 +0100

    ejercicio 2

---
+++ App6/App6/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class MainPage :MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new homepage());
+          
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new batpage());
+            IsPresented = false;
+        }
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new superpage());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked3(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new wonderpage());
+            IsPresented = false;
+        }
+
+    }
+}
