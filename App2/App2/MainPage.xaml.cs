c1fc9956e75a22cfd2733455e66de0ac80ed0b18
master <josemartin.lopez.blanco@gmail.com>
2019-02-07 20:25:29 +0100

    botoncito

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -14,23 +14,13 @@ namespace App2
             InitializeComponent();
         }
 
-        private Random rnd = new Random();
+        
 
         private async void Button_Clicked(object sender, EventArgs e)
-        {
+        { 
             await LabelText.RotateTo(360, 500, Easing.SinOut);
-
-            LabelText.Animate("ColorAnimation", (value) => LabelText.BackgroundColor = RandomColor(value));
-
             await LabelText.RotateTo(-360, 500, Easing.SpringOut);
 
-            
-        }
-
-        private Color RandomColor(double value)
-        {            
-            System.Diagnostics.Debug.WriteLine($"Value: {value}");                       
-            return new Color(rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble());
         }
 
     }
