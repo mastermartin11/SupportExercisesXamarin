bdf95a928ab1f510b4825d70f4641fa7b7b59316
master <josemartin.lopez.blanco@gmail.com>
2019-02-07 20:57:05 +0100

    todo hecho

---
+++ App10/App10/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        public void modifica(object sender, TextChangedEventArgs e)
+        {
+            
+            salida.Placeholder = entrada.Text;
+
+        }
+    }
+}
