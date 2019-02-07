92ed4d0b5c0668e2e4dffabe376977a1434e5d81
master <josemartin.lopez.blanco@gmail.com>
2019-02-07 10:27:10 +0100

    falta añadir paginas

---
+++ App7/App7/Classes/ListViewTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App7.Classes
+{
+    class ListViewTemplate
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int OrderNumber { get; set; }
+    }
+}
