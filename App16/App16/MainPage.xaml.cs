916a86c0f57f0fad01d8eb20017758006fc1c15e
master <josemartin.lopez.blanco@gmail.com>
2019-02-09 11:52:33 +0100

    boton hecho

---
+++ App16/App16/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App16
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void nuevoLabel(object sender, EventArgs e)
+        {
+            var label = new Label();
+            label.Text = "batman";
+
+            layout.Children.Add(label);
+
+
+        }
+    }
+}
