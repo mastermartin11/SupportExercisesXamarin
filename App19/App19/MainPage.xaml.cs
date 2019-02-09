115f7ea24a6049772fa186b28b44111a25319b9e
master <josemartin.lopez.blanco@gmail.com>
2019-02-09 13:24:42 +0100

    no completo

---
+++ App19/App19/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App19
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            activity.IsEnabled = true;
+            activity.IsRunning = true;
+            activity.IsVisible = true;
+
+
+            parar();
+        }
+
+        public void parar()
+        {
+            System.Threading.Thread.Sleep(30000);
+
+            activity.IsEnabled = false;
+            activity.IsRunning = false;
+            activity.IsVisible = false;
+        }
+
+
+    } 
+    }
