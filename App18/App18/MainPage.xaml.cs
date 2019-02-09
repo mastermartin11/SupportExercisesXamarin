0806f57aa9789bf0ab1cc59a3eb1b9cc9d8fbe29
master <josemartin.lopez.blanco@gmail.com>
2019-02-09 12:54:41 +0100

    hecho

---
+++ App18/App18/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App18
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result= await this.DisplayAlert("salir", "seguro que quieres salir", "si", "no");
+                if (result) await this.Navigation.PopAsync();
+            });
+            return true;
+        }
+    }
+}
