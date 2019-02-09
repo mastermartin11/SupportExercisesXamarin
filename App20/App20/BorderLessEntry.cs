2cc18a71c9e0ede9d223f49781bd097c7e8bed6c
master <josemartin.lopez.blanco@gmail.com>
2019-02-09 13:43:00 +0100

    x

---
+++ App20/App20/BorderLessEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+
+namespace App20
+{
+    public partial class BorderLessEntry : Entry
+    {
+        
+
+
+    }
+}
---
+++ App21/App21/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App21
+{
+    class CustomEntry:Entry
+    {
+    }
+}
+   
