ed399f3092bd75a773f2628f5dbd619a11ff41e9
master <josemartin.lopez.blanco@gmail.com>
2019-02-11 17:11:46 +0100

    en proceso

---
+++ App27/App27/Deportista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App27
+{
+    class Deportista
+    {
+
+        public int puntuacion { set; get; }
+        public String nombre { set; get; }
+        public String deporte { set; get; }
+        public int edad { set; get; }
+
+    }
+}
